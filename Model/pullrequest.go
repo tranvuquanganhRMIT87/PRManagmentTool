043836f3ee87ea3378ad9c4f083414bff67fcc09
Tran Vu Quang Anh <153184535+tranvuquanganhRMIT87@users.noreply.github.com>
2024-11-04 16:47:23 +0700

    refractor

--- Model/pullrequest.go
+++ Model/pullrequest.go
@@ -12,7 +12,6 @@ var Payload struct {
 			Login string `json:"login"`
 		} `json:"user"`
 	} `json:"pull_request"`
-	Events     string `json:"events"`
 	Repository struct {
 		FullName string `json:"full_name"`
 		HTMLURL  string `json:"html_url"`
